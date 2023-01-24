2f0cc8fcd892571fc4f2a7b642c147386e6ca678
effkey <kubpi2019@gmail.com>
2023-01-24 16:38:00 +0100

    Zmiany v2

--- rent-app/src/App.js
+++ rent-app/src/App.js
@@ -4,10 +4,9 @@ import { Navbar } from "./components/Navbar";
 import { Details } from "./pages/details/Details";
 import { Images } from "./pages/details/Images.js";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
-import { Nav } from "react-bootstrap";
+
 import { FetchProvider } from "./hooks/UseFetchContext";
 import { AddProduct } from "./pages/addProduct/AddProduct";
-import { Summary } from "./pages/summary/Summary";
 
 function App() {
   return (
@@ -23,7 +22,6 @@ function App() {
             {/* Routing z parametrem */}
             <Route path="/details/:id" element={<Details />} />
             <Route path="/details/:id/images" element={<Images />} />
-            {/* <Route path="/summary" element={<Summary />} /> */}
           </Routes>
         </ShoppingCartProvider>
       </FetchProvider>
